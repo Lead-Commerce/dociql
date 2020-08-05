db12d57db0f3c61e9361870423b56aa6e2e869fe
Josiah Nethery <josiah@leadcommerce.com>
2020-08-05 01:17:27 -0400

    feat(master): master Updating code for our specific use case to ignore introspection

--- app/dociql/fetch-schema.js
+++ app/dociql/fetch-schema.js
@@ -2,6 +2,21 @@ const graphql = require('graphql')
 const request = require("sync-request")
 
 const converter = require('graphql-2-json-schema');
+const { filter } = require('bluebird');
+
+const IGNORE = 'IGNORE INTROSPECTION';
+
+const filterFields = (fields) => {
+    if (fields !== null) {
+        return fields.filter(field => {
+            if (field.description) {
+                return !field.description.includes(IGNORE);
+            }
+            return true;
+        })
+    }
+    return null;
+}
 
 module.exports = function (graphUrl) {
 
@@ -13,7 +28,28 @@ module.exports = function (graphUrl) {
         json: requestBody
     }).getBody('utf8');
 
-    const introspectionResponse = JSON.parse(responseBody);    
+    const introspectionResponse = JSON.parse(responseBody);
+
+    // Filter out types marked for ignore
+    introspectionResponse.data.__schema.types = introspectionResponse.data.__schema.types
+    .filter(type => {
+        return !type.description.includes(IGNORE)
+    })
+    .map(object => {
+        // Filter out fields and inputFields marked for ignore
+        return {
+            ...object,
+            fields: filterFields(object.fields),
+            inputFields: filterFields(object.inputFields),
+        };
+    });
+    
+    // DEBUGGING
+    /* introspectionResponse.data.__schema.types.map((object, index) => {
+        if (object.name === 'OrderByUserInput') {
+            console.log(object);
+        }
+    }); */
 
     const jsonSchema = converter.fromIntrospectionQuery(introspectionResponse.data);
     const graphQLSchema = graphql.buildClientSchema(introspectionResponse.data, { assumeValid: true});
